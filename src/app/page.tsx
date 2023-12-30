e7e2b791b1611789f4b8918ea147ea0b22fdea0b
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-12-30 16:15:05 -0500

    Create components for link buttons and bg gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
+import { BgGradient } from "@/components/BgGradient";
+import { LinkButton } from "@/components/LinkButton";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="my-auto flex flex-col gap-4">
+    <main className="flex min-h-screen flex-col items-center text-neutral-200">
+      <div className="my-auto flex flex-col gap-4 max-w-sm">
+        <BgGradient />
 
-      <div className="flex flex-col place-items-start before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-purple-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-purple-700 before:dark:opacity-10 after:dark:from-purple-600 after:dark:via-yellow-700 after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1>Quan Nguyen</h1>
-        <p className="font-extralight text-xs">Software engineer at MSKCC</p>
-        <p className="font-extralight text-xs">Master&apos;s student at Penn Engineering (MCIT)</p>
-      </div>
+        <section className="flex flex-col gap-2">
+          <h1>Hi, I&apos;m Quan 👋</h1>
 
-      <div>
-        <h2 className="text-sm">Select work</h2>
-      </div>
+          <div className="font-extralight text-sm">
+            I&apos;m a software engineer at{' '}
+            <LinkButton
+              text="MSKCC"
+              href="https://www.mskcc.org/"
+              imgSrc="/logos/mskcc.png"
+              imgDimTw="w-2 h-3.5"
+            />
+            , where I help streamline cancer research processes.
+          </div>
+        </section>
 
-      <div>
-        <h2 className="text-sm">Socials</h2>
-      </div>
+        <section>
+          <h2>Select work</h2>
+        </section>
+
+        <section>
+          <h2>Connect</h2>
+        </section>
 
       </div>
     </main>
---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+
+/**
+ * @param text Text to display
+ * @param href External link
+ * @param imgSrc Path to image inside /public
+ * @param imgDimTw Tailwind CSS width and height
+ */
+export function LinkButton({ text, href, imgSrc, imgDimTw }: {
+  text: string,
+  href: string,
+  imgSrc: string,
+  imgDimTw: string,
+}) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="inline items-center gap-x-1 rounded-md bg-neutral-800 border border-neutral-600 px-1 py-0.5 text-white text-xs font-normal focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
+    >
+      <div className="inline-flex align-middle gap-1">
+        <div className={`relative ${imgDimTw}`}>
+          <Image
+            layout="fill"
+            objectFit="contain"
+            src={imgSrc}
+            alt={`${text} logo`}
+            className=""
+            />
+        </div>
+        <span>{text}</span>
+      </div>
+    </a>
+  )
+}
