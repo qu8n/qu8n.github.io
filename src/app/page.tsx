ee1f359da501806c3556f8275ba4f70db1a28a05
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-12-30 16:36:34 -0500

    Configure project with both ESLint and Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,34 @@
-import { BgGradient } from "@/components/BgGradient";
-import { LinkButton } from "@/components/LinkButton";
+import { BgGradient } from '@/components/BgGradient'
+import { LinkButton } from '@/components/LinkButton'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center text-neutral-200">
-      <div className="my-auto flex flex-col gap-4 max-w-sm">
+    <main className='flex min-h-screen flex-col items-center text-neutral-200'>
+      <div className='my-auto flex flex-col gap-4 max-w-sm'>
         <BgGradient />
 
-        <section className="flex flex-col gap-2">
+        <section className='flex flex-col gap-2'>
           <h1>Hi, I&apos;m Quan 👋</h1>
 
-          <div className="font-extralight text-sm">
+          <div className='font-extralight text-sm'>
             I&apos;m a software engineer at{' '}
             <LinkButton
-              text="MSKCC"
-              href="https://www.mskcc.org/"
-              imgSrc="/logos/mskcc.png"
-              imgDimTw="w-2 h-3.5"
+              text='MSKCC'
+              href='https://www.mskcc.org/'
+              imgSrc='/logos/mskcc.png'
+              imgDimTw='w-2 h-3.5'
             />
             , where I help streamline cancer research processes.
           </div>
         </section>
 
-        <section>
+        <section className='flex flex-col gap-2'>
           <h2>Select work</h2>
         </section>
 
-        <section>
+        <section className='flex flex-col gap-2'>
           <h2>Connect</h2>
         </section>
-
       </div>
     </main>
   )
